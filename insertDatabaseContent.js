9e7655c3df88215c0d6e50e82c0af9a0bc3fece4
Luca Campanella <luca.campanella1@gmail.com>
2020-08-03 17:01:44 +0200

    if exists updates

--- insertDatabaseContent.js
+++ insertDatabaseContent.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const config = require( "./config");
 
-const uri = 'mongodb://localhost/labelling_tool';
-// const uri = 'mongodb://heroku_wll30t81:u7m90co6idj3qrj24mqcg2u2vi@ds153304.mlab.com:53304/heroku_wll30t81';
+// const uri = 'mongodb://localhost/labelling_tool';
+const uri = 'mongodb://heroku_wll30t81:u7m90co6idj3qrj24mqcg2u2vi@ds153304.mlab.com:53304/heroku_wll30t81';
 mongoose.connect(uri, {useNewUrlParser:true, useCreateIndex: true, useUnifiedTopology:true});
 
 const connection = mongoose.connection;
@@ -16,37 +16,47 @@ function shuffleArray(array) {
     }
 }
 
+let articlesJson = require(`./json/articles`);
+
+//insert consecutive ids for paragraphs
+articlesJson = articlesJson
+    .filter(article => article.comments.length >= config.commentsPerArticle)
+    .map(article => {
+        article.paragraphs = article.paragraphs.map((par, index) => {
+            return {consecutiveID: index, text: par};
+        });
+
+        if(!article.stanceQuestion) {
+            article.stanceQuestion = "Is the article in favour or against the topic it's talking about?"
+        }
+
+        //comments are shuffled at insertion time
+        shuffleArray(article.comments);
+        return article;
+    });
+
 connection.once("open", () => {
     console.log("MongoDB database connection established successfully");
+
+    const articles = require(`./models/articles`);
+
 
     console.log("Creating database");
     connection.db.listCollections({name: 'articles'})
         .next(function(err, collinfo) {
             console.log("Collection articles");
-            const articles = require(`./models/articles`);
             if (collinfo) {
-                console.log("already exists, content");
-                articles.find({}).exec((err, res) => console.log(JSON.stringify(res)));
+                console.log("already exists, updating instead");
+                // articles.find({}).exec((err, res) => console.log(JSON.stringify(res)));
+                const allPromises = []
+                articlesJson.forEach(art => {
+                    allPromises.push(articles.findOneAndUpdate({articleID: art.articleID}, art).exec());
+                });
+                Promise.all(allPromises)
+                    .then(() => console.log("Updated succesfully"))
+                    .catch(err => console.log(err));
             } else {
                 console.log("doesn't exist, creating it");
-                let articlesJson = require(`./json/articles`);
-
-                //insert consecutive ids for paragraphs
-                articlesJson = articlesJson
-                    .filter(article => article.comments.length >= config.commentsPerArticle)
-                    .map(article => {
-                    article.paragraphs = article.paragraphs.map((par, index) => {
-                        return {consecutiveID: index, text: par};
-                    });
-
-                    if(!article.stanceQuestion) {
-                        article.stanceQuestion = "Is the article in favour or against the topic it's talking about?"
-                    }
-
-                    //comments are shuffled at insertion time
-                    shuffleArray(article.comments);
-                    return article;
-                });
 
                 articles.insertMany(articlesJson, function(err, result) {
                     if (err) {
