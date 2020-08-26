07ce6c076e1b9fc920be82ef83b23a000d72b6b9
Luca Campanella <luca.campanella1@gmail.com>
2020-08-26 11:06:28 +0200

    New preferences on interraters

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ config.baseLink = "https://mtc-emotion-stance.herokuapp.com";
 config.interrater = {};
 
 //number of duplicated labels per article and its respective comments
-config.interrater.labbellersPerArticle = 2;
+config.interrater.labbellersPerArticle = 30;
 
 //number of articles that will be multi-labelled depending on the labbellersPerArticle value
 //set to null in order to multilabel all the articles
-config.interrater.multiLabelledArticles = 30;
+config.interrater.multiLabelledArticles = null;
 
 module.exports = config;
