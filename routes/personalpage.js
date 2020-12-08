b75eb1800a1b6ba7836b03400f10392235b4572f
Luca Campanella <luca.campanella1@gmail.com>
2020-12-08 17:35:40 +0100

    Add the header stating licence and project to all files

--- routes/personalpage.js
+++ routes/personalpage.js
@@ -1,3 +1,21 @@
+// Copyright 2020-2021, ETH Zurich, Media Technology Center
+//
+// This file is part of Labels Collection Tool (LCT) at MTC, in the scope of the project
+// Emotion and Stance detection for German text.
+//
+// Labels Collection Tool (LCT) is a free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Labels Collection Tool (LCT) is distributed in the hope that it will be useful for similar projects,
+// but Labels Collection Tool (LCT); without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser Public License for more details.
+//
+// You should have received a copy of the GNU Lesser Public License
+// along with Labels Collection Tool (LCT). If not, see <https://www.gnu.org/licenses/>.
+
 const router = require('express').Router();
 const config = require( "../config");
 
