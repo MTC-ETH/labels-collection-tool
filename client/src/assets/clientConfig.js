61a76a09038e3db88306cb427fcf1cccfc6fd857
schneech <schneech@staff-ethz.ch>
2021-03-24 08:20:59 +0100

    Update copyright header

--- client/src/assets/clientConfig.js
+++ client/src/assets/clientConfig.js
@@ -1,20 +1,16 @@
-// Copyright 2020-2021, ETH Zurich, Media Technology Center
+//   Copyright 2021 ETH Zurich, Media Technology Center
 //
-// This file is part of Labels Collection Tool (LCT) at MTC, in the scope of the project
-// Emotion and Stance detection for German text.
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
 //
-// Labels Collection Tool (LCT) is a free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
+//        http://www.apache.org/licenses/LICENSE-2.0
 //
-// Labels Collection Tool (LCT) is distributed in the hope that it will be useful for similar projects,
-// but Labels Collection Tool (LCT); without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU Lesser Public License for more details.
-//
-// You should have received a copy of the GNU Lesser Public License
-// along with Labels Collection Tool (LCT). If not, see <https://www.gnu.org/licenses/>.
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
 
 const clientConfig = {};
 
